57f5c6ae94d95ad50dea481d5720f6270010f416
Bilawal Asghar <asgharbilawal6@gmail.com>
2021-01-29 18:26:37 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from time import strftime
+from tkinter import Label, Tk
+
+
+window = Tk()
+window.title("")
+window.geometry("200x80")
+window.configure(bg="black")  
+window.resizable(False, False)
+
+clock_label = Label(
+    window, bg="black", fg="white", font=("Times", 30, "bold"), relief="flat"
+)
+clock_label.place(x=20, y=20)
+
+
+def update_label():
+    
+    current_time = strftime("%H: %M: %S")
+    clock_label.configure(text=current_time)
+    clock_label.after(80, update_label)
+
+
+update_label()
+window.mainloop()
